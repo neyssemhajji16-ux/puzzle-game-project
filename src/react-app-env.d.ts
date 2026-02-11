2d2294c9099958eb088f374c9a0589987953f12c
neyssemhajji16-ux <neyssemhajji16@gmail.com>
2026-02-11 04:05:42 +0100

    update game

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,7 @@
-/// <reference types="react-scripts" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/puzzle-game-project/',
+})
